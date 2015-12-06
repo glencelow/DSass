2e7942af488c626afd9d941bf8f68d530d4c484e
glencelow <glencelow@hotmail.com>
2015-12-06 22:12:47 +0800

    comments for gameobject

--- DSassignment1/DSassignment1/GameObject.h
+++ DSassignment1/DSassignment1/GameObject.h
@@ -8,26 +8,25 @@ using std::string;
 
 
 
-class GameObject : public Dweller
+class GameObject : public Dweller //most important class (i think)
 {
 public:
 	GameObject();
 	~GameObject();
 
 	//getter
-	virtual string getName(void);
-	virtual int getCount();
+	virtual string getName(void);	//literally
+	virtual int getCount();			//literally
 
 
 private:
-	int count_;
-	virtual const string kName();
+	int count_;						//store number of object of this type instantited, dwe and weapon and outfit will + 1 to count
+	virtual const string kName();	
 
 protected:
 	GameObject(const string);
 
 };
-
 
 
 
