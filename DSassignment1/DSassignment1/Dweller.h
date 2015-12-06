9e064a46e2b2d1d9007fc6ebcf0a362559c46a64
glencelow <glencelow@hotmail.com>
2015-12-06 22:07:04 +0800

    added comments for dweller so many sia cher ><

--- DSassignment1/DSassignment1/Dweller.h
+++ DSassignment1/DSassignment1/Dweller.h
@@ -9,58 +9,56 @@
 using std::string;
 
 
-class Dweller
+class Dweller 
 {
 public:
 	Dweller();
 	~Dweller();
 
-	Dweller(const string&, const int&);
+	Dweller(const string&, const int&);// name then the special value
 
 	//getter
-	const int getSPECIAL();
-	const int getCurrentHealth();
-	const int getCurrentRadDamge();
-	const int getAttackDmg();
+	const int getSPECIAL();//literally
+	const int getCurrentHealth();//literally
+	const int getCurrentRadDamge();//literally
+	const int getAttackDmg(); // literally
 
 	//setter
-	void setPosition(const Vec2D&);
-	const Vec2D setPosition();
+	void setPosition(const Vec2D&); //literally
+	const Vec2D setPosition(); 
 	
 	//receive
-	void receiveHealthDamage(const int&);
-	void receiveRadDamge(const int&);
-	void receiveEquipmentDamage(const int&);
+	void receiveHealthDamage(const int&); //+hp
+	void receiveRadDamge(const int&); // +radiation
+	void receiveEquipmentDamage(const int&); //-equ durability
 
 	//add
-	void addStimpak(const int&);
-	void addRadAway(const int&);
+	void addStimpak(const int&); // +1 stimpak
+	void addRadAway(const int&); // +radiation
 
 
 	//use
-	void useStimpak();
-	void useRadAway();
+	void useStimpak(); // -1 stimpak
+	void useRadAway(); // -radiation
 
 	//assign
-	Outfit* assignOutfit(Outfit*);
+	Outfit* assignOutfit(Outfit*); 
 	Weapon* assignWeapon(Weapon*);
 
 	//other
-	bool isDead();
+	bool isDead(); //hp<0 death
 
 
 
 private:
-	int SPECIAL_;
-	int health_;
-	int radiation_;
-	int stimpak_;
-	int radaway_;
-	Outfit * outfit_;
-	Weapon * weapon_;
-	int SPECIAL_;
-	Vedc2D position_;
-
+	int SPECIAL_;				//attributes of dwe
+	int health_;				//current hp
+	int radiation_;				//current rad, start from 0 , determine what max hp a dwe can have 
+	int stimpak_;				//total amount, start at 0, heals dwe
+	int radaway_;				//start from 0 , - rad
+	Outfit * outfit_;			//what dwe wearing
+	Weapon * weapon_;			// what dwe weapon
+	Vec2D position_;			//literally
 
 
 
